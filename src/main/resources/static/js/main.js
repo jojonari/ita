86e71c80c1c50f1f6b1ff9176df07da78e6d0055
jojonari <xkgun@naver.com>
2021-02-07 23:32:04 +0900

    앱등록 form 작성 중

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,8 +2,27 @@ window.ita = new Vue({
     el: '#ita',
     data: {
         app: {
-            list: {
-                visible: true
+            manage: {
+                values: {
+                    appName: '',
+                    clientId: '',
+                    secretKey: '',
+                    partnerId: '',
+                    grantType: 'code',
+                    manageToken: 'refresh',
+                    operationLevel: 'qa',
+                    scope: []
+                },
+                options: {
+                    grantType: [{text: 'code', value: 'code'}, {text: 'credential', value: 'credential'}],
+                    manageToken: [{text: '갱신', value: 'refresh'}, {text: '유지', value: 'keep'}],
+                    operationLevel: [
+                        {text: 'QA', value: 'qa'},
+                        {text: 'AUTO', value: 'auto'},
+                        {text: 'DEV', value: 'dev'},
+                        {text: 'PROD', value: 'prod'}
+                    ]
+                }
             }
         },
         fields: ['first_name', 'last_name', 'age'],
@@ -20,42 +39,18 @@ window.ita = new Vue({
             {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
             {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
             {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
-            {isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson'},
             {isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney'}
         ]
+    },
+    computed: {
+        stateAppName() {
+            return this.app.manage.values.appName.length > 3
+        },
+        stateClientId() {
+            return this.app.manage.values.clientId.length > 16
+        },
+        stateSecretKey() {
+            return this.app.manage.values.secretKey.length > 16
+        }
     }
 })
